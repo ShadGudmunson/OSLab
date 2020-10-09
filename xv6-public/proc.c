125bee51e8c5eabfaeafe0692bb4156fbccdada9
nmtcasey <casey.haynes@student.nmt.edu>
2020-10-08 23:21:07 -0600

    Added code for nice.c program. Added priority printing to cps syscall.

--- xv6-public/proc.c
+++ xv6-public/proc.c
@@ -587,14 +587,14 @@ cps()
    sti();
    //Loop over process table looking for process with pid
    acquire(&ptable.lock);
-   cprintf("name \t pid \t state \t \n");
+   cprintf("name \t pid \t state \t priority\n");
    for(p = ptable.proc; p < &ptable.proc[NPROC]; p++){
       if(p->state == SLEEPING)
-         cprintf("%s \t %d \t SLEEPING \t \n", p->name, p->pid);
+         cprintf("%s \t %d \t SLEEPING \t %d\n", p->name, p->pid, p->nice);
       else if(p->state == RUNNING)
-         cprintf("%s \t %d \t RUNNING \t \n", p->name, p->pid);
+         cprintf("%s \t %d \t RUNNING \t %d\n", p->name, p->pid, p->nice);
       else if(p->state == RUNNABLE)
-         cprintf("%s \t %d \t RUNNABLE \t \n", p->name, p->pid);
+         cprintf("%s \t %d \t RUNNABLE \t %d\n", p->name, p->pid, p->nice);
    }
    release(&ptable.lock);
    return 22;
